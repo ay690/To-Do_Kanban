4473a0a90caa7c13213764db8e7d4af8edd34e91
Aniket Yadav <aniketyadav690@gmail.com>
2024-05-31 20:49:50 +0530

    update: pacakges and added icons in Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { GoPencil } from "react-icons/go";
 
 const Task = ({ task, moveTask, nextStatus }) => {
   return (
-    <div className="task">
-      <h3>{task.title}</h3>
-      <p>{task.description}</p>
-      {task.status === "Completed" && (
-        <p>Completed at: {new Date(task.completedAt).toLocaleString()}</p>
-      )}
-      <div>
-        {nextStatus && (
-          <button  onClick={() => moveTask(task.id, nextStatus)}>
-            Move to {nextStatus}
-          </button>
+    <>
+      <div className="task">
+        <div className="contents">
+          <div>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+          </div>
+          <div className="icons">
+            <GoPencil color="#72A0C1" />
+            <RiDeleteBin6Line color="red" />
+          </div>
+        </div>
+        {task.status === "Completed" && (
+          <p>Completed at: {new Date(task.completedAt).toLocaleString()}</p>
         )}
+        <div>
+          {nextStatus && (
+            <button onClick={() => moveTask(task.id, nextStatus)}>
+              Move to {nextStatus}
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
